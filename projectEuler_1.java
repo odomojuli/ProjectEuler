f42b01aaaf3e0ead7b0f6a3ab6c2f84594ca25a3
Juli Odomo <odomojuli@gmail.com>
2016-04-30 19:30:11 -0700

    Update projectEuler_1.java
    changed variable names

--- projectEuler_1.java
+++ projectEuler_1.java
@@ -3,7 +3,8 @@
 //bruteforce
 public static void main(String[] args) {
     int sum = 0;
-    for (int i = 3; i < 1000; i++) {
+    int max = 1000;
+    for (int i = 3; i < max; i++) {
         if (i % 3 == 0 || i % 5 == 0) {
             sum += i;
         }
@@ -16,7 +17,8 @@ public static void main(String[] args) {
 //      = 333 + 200 + 66
 public static void main(String[] args) {
     int sum = 0;
-    for (int i = 3; i < 1000; i++) {
+    int max = 1000;
+    for (int i = 3; i < max; i++) {
         if (i % 3 == 0 || i % 5 == 0) {
             sum += i;
         }
@@ -29,16 +31,16 @@ public static void main(String[] args) {
 // Proof: (n/2)*(A1 + An) = n(A1 + An)/2
 
 public static void main(String[] args) {
-    int nr = 1000;
-    nr--;
-    int x3 = nr/3;
-    int x5 = nr/5;
-    int x15 = nr/15;
+    int max = 1000;
+    max--;
+    int x3 = max/3;
+    int x5 = max/5;
+    int x15 = max/15;
 
-    long sum1 = 3*x3*(x3+1); 
-    long sum2 = 5*x5*(x5+1);
-    long sum3 = 15*x15*(x15+1);
+    long sum3 = 3*x3*(x3+1); 
+    long sum5 = 5*x5*(x5+1);
+    long sum15 = 15*x15*(x15+1);
 
-    long sum = (sum1+sum2-sum3)/2;
+    long sum = (sum3+sum5-sum15)/2;
     System.out.println(sum);
 }
